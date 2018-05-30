914499630732c51abf75acc044409a0be62b52eb
yaliun <yaliun@yaliun-PC>
2018-05-30 18:11:04 +0900

    2

--- src/main/java/net/yaliun/tosima/HomeController.java
+++ src/main/java/net/yaliun/tosima/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}. test-1", locale);
+		logger.info("Welcome home! The client locale is {}. test-2", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
